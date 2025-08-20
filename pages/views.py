2da6856805cc8a9e8f23ed26151d057170b5a4bb
jayounghoyos <jayoungh@eafit.edu.co>
2025-08-20 08:15:08 -0500

    vista vuelos

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views import View
 # Create your views here.
-
+    
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
 
